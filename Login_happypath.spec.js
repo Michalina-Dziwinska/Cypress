70ad31f74a33d435fffc0b9ac10502523e7633df
Michalina-Dziwinska <83969041+Michalina-Dziwinska@users.noreply.github.com>
2021-10-13 10:17:03 +0200

    Update Login_happypath.spec.js

--- Login_happypath.spec.js
+++ Login_happypath.spec.js
@@ -1,7 +1,7 @@
 
 describe('Login', () => {
   it('Open form', () => {
-    cy.visit("https://husky-cognito.dvwf5uw58zw69.amplifyapp.com/")
+    cy.visit("link")
     cy.wait(4000)
 	cy.get('[id="login-modal-button"]').click()
 	cy.get('[id="email"]').clear().type('jekir94599@stvbz.com')
